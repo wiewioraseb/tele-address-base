add9591e655df1de91f837fdecc470d709e0e3c8
wiewioraseb <wiewiora.seb@gmail.com>
2018-06-23 19:50:10 +0200

    telephone and email validator

--- client-side/src/elm/View.elm
+++ client-side/src/elm/View.elm
@@ -7,6 +7,7 @@ import Html.Events exposing (onClick, onInput)
 import List exposing (head, take)
 import Model exposing ( Model )
 import Msg exposing ( Msg(..) )
+import Regex exposing (regex)
 import String exposing (toList)
 
 
@@ -28,8 +29,10 @@ view model =
 --        , input [ placeholder "Birthday", onInput Birthday  ] [ ]
         , br [] []
         , input [ placeholder "Telephone", onInput Telephone  ] [ ]
+        , telephoneValidation model.telephone
         , br [] []
         , input [ placeholder "Email", onInput Email  ] [ ]
+        , emailValidation model.email
         , br [] []
         , checkbox AcceptCompanyRules "Accept company rules."
         ]
@@ -54,6 +57,32 @@ minimumLengthValidation fieldTextContent =
                 ("green", "OK")
             else
                 ("red", "Data provided is too short")
+    in
+        div [ style [("color", color)] ] [ text message ]
+
+telephoneValidation : String -> Html msg
+telephoneValidation fieldTextContent =
+    let
+        telephoneNumber = (String.toList fieldTextContent)
+            |> List.filter (\x-> x /= '-')
+            |> List.filter (\x-> not (Char.isDigit x))
+        (color, message) =
+            if (List.length telephoneNumber) == 0 then
+                ("green", "OK")
+            else
+                ("red", "Telephone number can't consist of alphabetic characters and special sign except for '-'")
+    in
+        div [ style [("color", color)] ] [ text message ]
+
+emailValidation : String -> Html msg
+emailValidation fieldTextContent =
+    let
+        (color, message) =
+            -- basic regex email validation
+            if Regex.contains (regex "^\\S+@\\S+\\.\\S+$") fieldTextContent then
+                ("green", "OK")
+            else
+                ("red", "Please, provide a valid email address")
     in
         div [ style [("color", color)] ] [ text message ]
 
